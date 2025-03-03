738069d96676ff55ffa465776504b3be109480a4
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2025-03-03 09:41:58 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,17 +1,18 @@
 def get_menu_option():
-  '''
-  Should print a menu with the following options:
+  
+  valasztas = input=("""
   1. Human vs Human
   2. Random AI vs Random AI
   3. Human vs Random AI
-  4. Human vs Unbeatable AI
+  4. Human vs Unbeatable AI""")
 
+"""
   The function should return a number between 1-4.
   If the user will enter invalid data (for example 5), than a message will appear
   asking to input a new value.
-  '''
-  pass
 
+  pass
+"""
 
 if __name__ == "__main__":
     # run this file to test you have implemented correctly the function
