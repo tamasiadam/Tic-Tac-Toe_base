00d6d4ecccf98f0310d6f318bcd49bf55eb5ed2e
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2025-03-05 13:25:39 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,25 +6,15 @@ def get_menu_option():
   3. Human vs Random AI
   4. Human vs Unbeatable AI
   
-  Válassz:"""))
+Select gamemode: """))
 
   if valasztas < 5:
-    print(f"{valasztas}. mód sikeresen kiválasztva! ")
+    print(f"Mode {valasztas} selected! ")
     return valasztas
   else:
     ValueError
-    print("Hiba! Kérlek adj meg egy számot 1-től 4-ig!")
+    print("\nError! Please select a number from 1 to 4.")
     get_menu_option()
-     
-"""
-  The function should return a number between 1-4.
-  If the user will enter invalid data (for example 5), than a message will appear
-  asking to input a new value.
-
-  pass
-"""
 
 if __name__ == "__main__":
-    # run this file to test you have implemented correctly the function
     option = get_menu_option()
-
