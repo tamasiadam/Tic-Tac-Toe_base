812f43d3f497e6a83df204a64b3ad93b600855bd
LLevente <ladanyi.levente@diak.szbi-pg.hu>
2025-03-10 09:18:37 +0100

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,20 +1,21 @@
 def get_menu_option():
-  
-  valasztas = int(input("""
+    valasztas = int(input("""
   1. Human vs Human
   2. Random AI vs Random AI
   3. Human vs Random AI
-  4. Human vs Unbeatable AI
-  
-Select gamemode: """))
+  4. Human vs Unbeatable AI\n
+  Please select a gamemode: """))
 
-  if valasztas < 5:
-    print(f"Mode {valasztas} selected! ")
-    return valasztas
-  else:
-    ValueError
-    print("\nError! Please select a number from 1 to 4.")
-    get_menu_option()
+    if valasztas < 5:
+        print(f"Mode {valasztas} selected!")
+        return valasztas
+    else:
+        ValueError
+        print("\nError! Please select a number from 1 to 4!")
+        get_menu_option()
+
 
 if __name__ == "__main__":
     option = get_menu_option()
+
+    
