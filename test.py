3db8a08d1d3ba30fc15398d0a950b867a4881973
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2025-03-26 14:09:18 +0100

    k

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ from menu import get_menu_option
 
 
 def main():
-    # The main function
+
     gamemode = get_menu_option()
     board = get_empty_board()
     decoration = board_decoration(board)
     symbol_1, symbol_2 = symbol_select_human_vs_human()
-    full = isFull(board, symbol_1, symbol_2)  # The function that starts the game is also in here.
+    full = isFull(board, symbol_1, symbol_2)
 
 
 def symbol_select_human_vs_human():
-    # This function decides the players' symbols
+    
     while True:
         symbol_1 = input("\nPlayer 1 choose your symbol (X or O): ").upper()
         if symbol_1 == "X":
@@ -30,9 +30,8 @@ def symbol_select_human_vs_human():
 
 
 def start(board, symbol_1, symbol_2, count):
-    # This function starts the game.
 
-    # Decides the turn
+
     if count % 2 == 0:
         player = symbol_1
     elif count % 2 == 1:
@@ -43,7 +42,7 @@ def start(board, symbol_1, symbol_2, count):
     column = int(input("Pick a column:"
                        "[left column: enter 0, middle column: enter 1, right column enter 2]"))
 
-    # Check if players' selection is out of range
+
     while (row > 2 or row < 0) or (column > 2 or column < 0):
         outOfBoard(row, column)
         row = int(input("Pick a row[upper row:"
@@ -51,7 +50,7 @@ def start(board, symbol_1, symbol_2, count):
         column = int(input("Pick a column:"
                            "[left column: enter 0, middle column: enter 1, right column enter 2]"))
 
-        # Check if the square is already filled
+
     while (board[row][column] == symbol_1) or (board[row][column] == symbol_2):
         filled = illegal(board, symbol_1, symbol_2, row, column)
         row = int(input("Pick a row[upper row:"
@@ -59,7 +58,6 @@ def start(board, symbol_1, symbol_2, count):
         column = int(input("Pick a column:"
                            "[left column: enter 0, middle column: enter 1, right column enter 2]"))
 
-        # Locates player's symbol on the board
     if player == symbol_1:
         board[row][column] = symbol_1
 
@@ -72,7 +70,7 @@ def start(board, symbol_1, symbol_2, count):
 def isFull(board, symbol_1, symbol_2):
     count = 1
     winner = True
-    # This function check if the board is full
+
     while count < 10 and winner == True:
         gaming = start(board, symbol_1, symbol_2, count)
         pretty = board_decoration(board)
@@ -82,25 +80,25 @@ def isFull(board, symbol_1, symbol_2):
             if winner == True:
                 print("There is a tie. ")
 
-        # Check if here is a winner
+
         winner = isWinner(board, symbol_1, symbol_2, count)
         count += 1
     if winner == False:
         print("Game over.")
 
-    # This is function gives a report
+
     report(count, winner, symbol_1, symbol_2)
 
 
 def outOfBoard(row, column):
-    # This function tells the players that their selection is out of range
+
     print("Out of boarder. Pick another one. ")
 
 
 def isWinner(board, symbol_1, symbol_2, count):
-    # This function checks if any winner is winning
+
     winner = True
-    # Check the rows
+
     for row in range(0, 3):
         if (board[row][0] == board[row][1] == board[row][2] == symbol_1):
             winner = False
@@ -110,7 +108,6 @@ def isWinner(board, symbol_1, symbol_2, count):
             winner = False
             print("Player " + symbol_2 + ", you won!")
 
-    # Check the columns
     for col in range(0, 3):
         if (board[0][col] == board[1][col] == board[2][col] == symbol_1):
             winner = False
@@ -119,7 +116,7 @@ def isWinner(board, symbol_1, symbol_2, count):
             winner = False
             print("Player " + symbol_2 + ", you won!")
 
-    # Check the diagnoals
+
     if board[0][0] == board[1][1] == board[2][2] == symbol_1:
         winner = False
         print("Player " + symbol_1 + ", you won!")
